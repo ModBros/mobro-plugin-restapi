48bfb98cc2d134097a3441b6c20dbf1100226708
Manfred Graf <manfred.graf91@gmail.com>
2024-06-25 22:44:09 +0200

    chore: initialized project

---
+++ Plugin.cs
@@ -0,0 +1,53 @@
+using FastEndpoints;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MoBro.Plugin.SDK;
+using MoBro.Plugin.SDK.Services;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
+
+namespace MoBro.Plugin.RestApi;
+
+public sealed class Plugin : IMoBroPlugin, IDisposable
+{
+  private readonly ILogger _logger;
+  private readonly IMoBroService _mobro;
+  private readonly IMoBroSettings _settings;
+
+  private readonly CancellationTokenSource _cancellationTokenSource;
+
+  public Plugin(IMoBroService mobro, IMoBroSettings settings, ILogger logger)
+  {
+    _logger = logger;
+    _mobro = mobro;
+    _settings = settings;
+    _cancellationTokenSource = new CancellationTokenSource();
+  }
+
+  public void Init()
+  {
+    var port = _settings.GetValue<string>("port");
+
+    var builder = WebApplication.CreateBuilder();
+
+    builder.WebHost.UseUrls($"http://localhost:{port}");
+    builder.Services.AddFastEndpoints();
+    builder.Services.AddSingleton(_logger);
+    builder.Services.AddSingleton(_mobro);
+    builder.Services.AddSingleton(_settings);
+
+    var app = builder.Build();
+
+    app.UseDefaultExceptionHandler();
+    app.UseFastEndpoints();
+
+    // TODO handle error and forward to service
+    app.RunAsync(_cancellationTokenSource.Token);
+  }
+
+  public void Dispose()
+  {
+    _cancellationTokenSource.Dispose();
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using MoBro.Plugin.RestApi;
+using MoBro.Plugin.SDK;
+
+// create and start the plugin to test it locally
+var plugin = MoBroPluginBuilder
+  .Create<Plugin>()
+  .WithSetting("port", "8080")
+  .Build();
+
+// prevent the program from exiting immediately
+Console.ReadLine();
